529aed7aee66a4a521fc241a44893b1b532edfff
prudhvi-1122 <prudhwi1998@gmail.com>
2025-03-14 12:42:00 +0530

    eks

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -34,6 +34,7 @@ resource "aws_eks_addon" "addons" {
   cluster_name  = aws_eks_cluster.main.name
   addon_name    = each.key
   addon_version = each.value
+  resolve_conflicts_on_create = "OVERWRITE"
 }
 
 
