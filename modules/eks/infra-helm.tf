b071323622496499d1a08a9196324d4281df485d
prudhvi-1122 <prudhwi1998@gmail.com>
2025-03-22 22:24:48 +0530

    helm -secret store

--- modules/eks/infra-helm.tf
+++ modules/eks/infra-helm.tf
@@ -4,6 +4,7 @@ resource "null_resource" "kube-config" {
   provisioner "local-exec" {
     command =<<EOF
 aws eks update-kubeconfig --name ${var.env}-eks
+kubectl apply -f /opt/vault-token.yaml
 EOF
   }
 }
@@ -17,4 +18,31 @@ resource "helm_release" "external-secrets" {
   repository = "https://charts.external-secrets.io"
   chart      = "external-secrets"
   namespace  = "kube-system"
+  wait       = true
+}
+
+resource "null_resource" "external-secrets-store" {
+  depends_on = [helm_release.external-secrets]
+
+  provisioner "local-exec" {
+    command =<<EOF
+kubectl apply -f - <<EOK
+apiVersion: external-secrets.io/v1beta1
+kind: ClusterSecretStore
+metadata:
+  name: vault-backend
+spec:
+  provider:
+    vault:
+      server: "http://vault-internal.devrobo.online:8200/"
+      path: "roboshop-${var.env}"
+      version: "v2"
+      auth:
+        tokenSecretRef:
+          name: "vault-token"
+          key: "token"
+          namespace: kube-system
+EOK
+EOF
+  }
 }
